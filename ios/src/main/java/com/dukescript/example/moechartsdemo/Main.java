3e2493dd0e80123e7064137df06fefc1641225b6
Duke Script <dukescript@eppleton.de>
2017-09-22 09:08:24 +0200

    Adding Multi OS Engine subproject

---
+++ ios/src/main/java/com/dukescript/example/moechartsdemo/Main.java
@@ -0,0 +1,146 @@
+package com.dukescript.example.moechartsdemo;
+
+import apple.NSObject;
+import apple.coregraphics.struct.CGPoint;
+import apple.coregraphics.struct.CGRect;
+import apple.coregraphics.struct.CGSize;
+import apple.foundation.NSDictionary;
+import apple.foundation.NSURL;
+import apple.foundation.NSURLRequest;
+import apple.uikit.UIApplication;
+import apple.uikit.UIButton;
+import apple.uikit.UIColor;
+import apple.uikit.UILabel;
+import apple.uikit.UIScreen;
+import apple.uikit.UIViewController;
+import apple.uikit.UIWebView;
+import apple.uikit.UIWindow;
+import apple.uikit.c.UIKit;
+import apple.uikit.enums.UIButtonType;
+import apple.uikit.enums.UIControlEvents;
+import apple.uikit.enums.UIControlState;
+import apple.uikit.enums.UITextAlignment;
+import apple.uikit.enums.UIViewAutoresizing;
+import apple.uikit.protocol.UIApplicationDelegate;
+
+import org.moe.natj.general.NatJ;
+import org.moe.natj.general.Pointer;
+import org.moe.natj.general.ann.Runtime;
+import org.moe.natj.general.ann.RegisterOnStartup;
+import org.moe.natj.objc.ObjCRuntime;
+import org.moe.natj.objc.ann.ObjCClassName;
+import org.moe.natj.objc.ann.Selector;
+
+@RegisterOnStartup
+public class Main extends NSObject implements UIApplicationDelegate {
+
+    private UIButton left;
+    private UIButton right;
+    private UIWebView webView;
+    private UILabel label;
+
+    public static void main(String[] args) {
+        UIKit.UIApplicationMain(0, null, null, Main.class.getName());
+    }
+
+    @Selector("alloc")
+    public static native Main alloc();
+
+    protected Main(Pointer peer) {
+        super(peer);
+    }
+
+    private UIWindow window;
+
+    @Override
+    public boolean applicationDidFinishLaunchingWithOptions(UIApplication application, NSDictionary launchOptions) {
+        CGRect bounds = UIScreen.mainScreen().bounds();
+        double tenthHeight = bounds.size().height() / 10;
+        CGSize buttonSize = new CGSize(bounds.size().width() / 3, tenthHeight);
+        left = UIButton.buttonWithType(UIButtonType.RoundedRect);
+        left.setFrame(new CGRect(bounds.origin(), buttonSize));
+        left.setTitleForState("Left", UIControlState.Normal);
+        left.setTitleColorForState(UIColor.whiteColor(), UIControlState.Normal);
+        left.setBackgroundColor(UIColor.blueColor());
+        left.addTargetActionForControlEvents((source, event) -> {
+            label.setText("Left");
+        }, UIControlEvents.TouchDown);
+
+        right = UIButton.buttonWithType(UIButtonType.RoundedRect);
+        right.setFrame(new CGRect(new CGPoint(bounds.size().width() * 2 / 3, bounds.origin().y()), buttonSize));
+        right.setTitleForState("Right", UIControlState.Normal);
+        right.setTitleColorForState(UIColor.whiteColor(), UIControlState.Normal);
+        right.setBackgroundColor(UIColor.blueColor());
+        right.addTargetActionForControlEvents((source, event) -> {
+            label.setText("Right");
+        }, UIControlEvents.TouchDown);
+
+        CGRect webViewBounds = new CGRect(new CGPoint(0, tenthHeight), new CGSize(bounds.size().width(), bounds.size().height() - 2 * tenthHeight));
+        webView = UIWebView.alloc().initWithFrame(webViewBounds);
+        //webView.setDelegate(delegate);
+        webView.setAutoresizingMask(UIViewAutoresizing.FlexibleBottomMargin | UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleLeftMargin | UIViewAutoresizing.FlexibleRightMargin | UIViewAutoresizing.FlexibleTopMargin | UIViewAutoresizing.FlexibleWidth);
+
+        label = UILabel.alloc().initWithFrame(new CGRect(new CGPoint(0, bounds.size().height() - tenthHeight), new CGSize(bounds.size().width(), tenthHeight)));
+        label.setText("Click the chart!");
+        label.setTextAlignment(UITextAlignment.Center);
+
+        window.setRootViewController(MainController.alloc());
+        window.setBackgroundColor(UIColor.whiteColor());
+        window.rootViewController().setView(webView);
+
+        window.addSubview(left);
+        window.addSubview(right);
+        window.addSubview(webView);
+        window.addSubview(label);
+        window.makeKeyAndVisible();
+
+        NSURLRequest req = NSURLRequest.requestWithURL(NSURL.URLWithString("https://yahoo.com"));
+        webView.loadRequest(req);
+
+        return true;
+    }
+
+    @Override
+    public void setWindow(UIWindow value) {
+        window = value;
+    }
+
+    @Override
+    public UIWindow window() {
+        return window;
+    }
+
+
+    @Runtime(value = ObjCRuntime.class)
+    @RegisterOnStartup
+    @ObjCClassName(value = "MainController")
+    static final class MainController extends UIViewController {
+
+        static {
+            NatJ.register();
+        }
+
+        protected MainController(Pointer pntr) {
+            super(pntr);
+        }
+
+        @Selector(value = "alloc")
+        public static native MainController alloc();
+
+        @Override
+        public boolean prefersStatusBarHidden() {
+            return true;
+        }
+
+        @Override
+        public boolean shouldAutorotate() {
+            return true;
+        }
+
+        @Override
+        public boolean shouldAutomaticallyForwardRotationMethods() {
+            return false;
+        }
+    }
+
+}
